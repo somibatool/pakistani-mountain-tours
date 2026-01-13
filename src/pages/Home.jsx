f19a0a40037b9c6c2a24f1a9984c6a63f05f8c16
somi batool <somibatool44@gmail.com>
2026-01-13 12:53:36 +0500

    Remove all i.jpg references

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,4 @@
 import React from "react";
-import Hero from "../components/Hero/Hero";
-import NatureVid from "../assets/video/main.mp4";
-import BlogsComp from "../components/Blogs/BlogsComp";
 import Places from "../components/Places/Places";
 import Testimonial from "../components/Testimonial/Testimonial";
 import Banner from "../components/Banner/Banner";
